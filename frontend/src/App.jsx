31314238c4cb654e7827cf62b785d9f13109db07
CKepperling <ckepperling86@gmail.com>
2026-09-22 02:07:28 -0400

    small change on main to make sure the page loaded in correctly

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import ProtectedRoute from "./components/ProtectedRoute";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider } from "./context/AuthContext.jsx";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Review from "./pages/Review";
